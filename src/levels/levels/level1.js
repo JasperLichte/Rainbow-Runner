13640056d303817d6a165c27be6410c1b9a8b017
Harun Can <h.can@NBhcan.geschaeftsstelle.cc-egov.de>
2019-01-12 13:49:01 +0100

    Change level1

--- src/levels/levels/level1.js
+++ src/levels/levels/level1.js
@@ -4,18 +4,19 @@ const w = symbols['wall'];
 const c = symbols['coin'];
 const x = symbols['void'];
 const l = symbols['lava'];
+const s = symbols['spikes'];
 
 export default [
   [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
   [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
   [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
   [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
-  [x,x,x,x,c,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
-  [x,x,x,x,w,x,x,x,x,x,c,x,x,x,x,x,x,x,x,x,x,x,x,x,],
-  [x,x,x,x,x,x,x,x,w,l,l,l,w,x,x,x,x,x,x,x,x,x,x,x,],
-  [x,x,x,x,x,x,x,x,x,w,w,w,x,x,x,w,w,x,x,x,x,x,x,x,],
+  [x,x,x,x,x,x,x,c,x,s,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
+  [x,x,x,x,x,x,x,w,w,w,w,x,x,c,c,x,x,x,x,x,x,x,x,x,],
   [x,x,x,x,x,x,x,x,x,x,x,x,x,w,w,x,x,x,x,x,x,x,x,x,],
-  [x,x,x,x,x,x,x,x,x,x,c,c,x,x,x,x,x,x,x,x,x,x,x,x,],
+  [x,x,x,x,x,x,x,x,x,w,w,x,x,x,x,x,x,x,x,x,x,x,x,x,],
+  [x,x,x,x,x,w,w,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
+  [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
   [w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,],
   [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,],
 ];
