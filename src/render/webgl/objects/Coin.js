ceb81c58660615f462c0ad568f78a32ace6985d3
JasperLichte <contact@jasper-lichte.com>
2019-01-13 13:29:15 +0100

    Refactoring and Animate coin

--- src/render/webgl/objects/Coin.js
+++ src/render/webgl/objects/Coin.js
@@ -4,17 +4,28 @@ import colors from './../colors.js';
 export default class Coin {
 
   constructor() {    
-    this._geometry = new THREE.BoxGeometry(0.85, 0.85, 0.85);
+    this._geometry = new THREE.TorusGeometry(0.2, 0.05, 16, 100);
     this._material = new THREE.MeshBasicMaterial({color: colors.objects.coin});
     this._object = new THREE.Mesh(this._geometry, this._material);
 
+    this._object.rotation.y = 90;
+
     //------------
     this.getObject = this.getObject.bind(this);
+    this._rotate = this._rotate.bind(this);
     //------------
+
+    this._rotate();
   }
 
   getObject() {
     return this._object;
+  }
+
+  _rotate() {
+    this._object.rotation.y += 0.01;    
+
+    requestAnimationFrame(this._rotate);
   }
 
 }
