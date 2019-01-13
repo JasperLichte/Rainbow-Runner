384983c8c7dca508413b60dcf1de1b8457dfa4c3
JasperLichte <contact@jasper-lichte.com>
2019-01-13 21:35:15 +0100

    Add menu and sliding animation

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ import levels from './levels/levels.js';
 import ThreeHelper from './render/webgl/ThreeHelper.js';
 import { repeatedArray } from './func/generators.js';
 
-const threeHelper = new ThreeHelper();
-const nextLevelGenerator = repeatedArray(levels);
-
-render(threeHelper, nextLevelGenerator.next().value)
-document.getElementById('next-level-btn').addEventListener('click', _ => {
-  render(threeHelper, nextLevelGenerator.next().value)
-})
+document.querySelector('#slideAnimation .play-btn').addEventListener('click', function() {
+  const threeHelper = new ThreeHelper();
+  const nextLevelGenerator = repeatedArray(levels);
+  render(threeHelper, nextLevelGenerator.next().value);
+  
+  const el = document.getElementById('slideAnimation');
+  el.innerHTML = '';
+  el.style.transform = 'scale(0)';
+  
+  document.getElementById('next-level-btn').addEventListener('click', _ => {
+    render(threeHelper, nextLevelGenerator.next().value)
+  });
+});
