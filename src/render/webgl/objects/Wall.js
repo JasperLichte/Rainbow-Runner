4c9b07268fc1648aa2d9c0abf33ec1cb11bfa513
JasperLichte <contact@jasper-lichte.com>
2019-01-13 12:41:37 +0100

    Change Wall

--- src/render/webgl/objects/Wall.js
+++ src/render/webgl/objects/Wall.js
@@ -6,7 +6,13 @@ import colors from './../colors.js';
 export default class Wall extends Cube {
 
   constructor() {
-    super(new THREE.Color(colors.objects.wall));
+    super(new THREE.Color(colors.objects.wall.body));
+
+    const geo = new THREE.EdgesGeometry(this._cube.geometry);
+    const mat = new THREE.LineBasicMaterial({color: colors.objects.wall.border, linewidth: 1});
+    const wireframe = new THREE.LineSegments(geo, mat);
+    wireframe.renderOrder = 1;
+    this._cube.add(wireframe);
   }
 
 }
