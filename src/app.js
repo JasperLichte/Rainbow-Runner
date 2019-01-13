a3f55f53b16f568fb94d3c0ca9f43f38f4b972f3
JasperLichte <contact@jasper-lichte.com>
2019-01-13 20:26:58 +0100

    Refactoring

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import render from './render/webgl/render.js';
 import levels from './levels/levels.js';
 import ThreeHelper from './render/webgl/ThreeHelper.js';
+import { repeatedArray } from './func/generators.js';
 
 const threeHelper = new ThreeHelper();
 const nextLevelGenerator = repeatedArray(levels);
@@ -9,10 +10,3 @@ render(threeHelper, nextLevelGenerator.next().value)
 document.getElementById('next-level-btn').addEventListener('click', _ => {
   render(threeHelper, nextLevelGenerator.next().value)
 })
-
-function* repeatedArray(arr) {
-  let index = 0;
-  while (true) {
-    yield arr[index++ % arr.length];
-  }
-}
