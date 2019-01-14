c40d116646bd24d8b7f3b2cf01defde981a3d485
JasperLichte <contact@jasper-lichte.com>
2019-01-14 12:54:48 +0100

    Add 2d rendering engine

--- src/app.js
+++ src/app.js
@@ -1,18 +1,34 @@
-import render from './render/webgl/render.js';
 import levels from './levels/levels.js';
 import ThreeHelper from './render/webgl/ThreeHelper.js';
+import render3d from './render/webgl/render.js';
+import DomHelper from './render/dom/DomHelper.js';
+import render2d from './render/dom/render.js';
 import { repeatedArray } from './func/generators.js';
 
-document.querySelector('#slideAnimation .play-btn').addEventListener('click', function() {
-  const threeHelper = new ThreeHelper();
-  const nextLevelGenerator = repeatedArray(levels);
-  render(threeHelper, nextLevelGenerator.next().value);
-  
-  const el = document.getElementById('slideAnimation');
-  el.innerHTML = '';
-  el.style.transform = 'scale(0)';
-  
-  document.getElementById('next-level-btn').addEventListener('click', _ => {
-    render(threeHelper, nextLevelGenerator.next().value)
+const nextLevelGenerator = repeatedArray(levels);
+const level1 = nextLevelGenerator.next().value;
+
+document.querySelectorAll('#slideAnimation .play-btn').forEach(btn => {
+  btn.addEventListener('click', function() {
+    const mode = btn.getAttribute('data-mode');
+
+    let helper = null;
+    let render = null;
+    if (mode === '3d') {
+      helper = new ThreeHelper();
+      render = render3d;
+    } else {
+      helper = new DomHelper(document.getElementById('main'));
+      render = render2d;
+    }
+    render(helper, level1);    
+    
+    const el = document.getElementById('slideAnimation');
+    el.innerHTML = '';
+    el.style.transform = 'scale(0)';
+    
+    document.getElementById('next-level-btn').addEventListener('click', _ => {
+      render(helper, nextLevelGenerator.next().value)
+    });
   });
 });
