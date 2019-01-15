3f633448d8156f3260b85ec8973f1075b384eecb
JasperLichte <contact@jasper-lichte.com>
2019-01-15 14:20:48 +0100

    Refactor Cube

--- src/render/webgl/objects/Wall.js
+++ src/render/webgl/objects/Wall.js
@@ -6,13 +6,17 @@ import colors from './../../colors.js';
 export default class Wall extends Cube {
 
   constructor() {
-    super(new THREE.Color(colors['objects-wall-body']));
+    super();
+    this._geometry = new THREE.BoxGeometry(0.85, 0.85, 0.85);
+    this._material = new THREE.MeshBasicMaterial({color: new THREE.Color(colors['objects-wall-body'])});
+    this._object = new THREE.Mesh(this._geometry, this._material);
 
     const wireframe = new THREE.LineSegments(
       new THREE.EdgesGeometry(this._object.geometry), 
       new THREE.LineBasicMaterial({color: colors['objects-wall-border'], linewidth: 1})
     );
     wireframe.renderOrder = 1;
+
     this._object.add(wireframe);
   }
 
