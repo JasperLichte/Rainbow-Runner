2aa1eec83450767830ccf33e25ebc73c2c9afdcc
JasperLichte <contact@jasper-lichte.com>
2019-01-20 20:50:59 +0100

    Fix global keyboard event

--- src/func/funcs.js
+++ src/func/funcs.js
@@ -1,6 +1,6 @@
 export const calcGravity = (m, g) => (m / 100000) * g;
 
 export const preventDefaultEvents = _ => {
-  const preventDefault = event => event.preventDefault();
-  window.addEventListener('keydown', preventDefault);
+  const preventDefault = e => e.preventDefault();
+  window.addEventListener('keydown', e => e.code === 'Space' && preventDefault(e));
 };
