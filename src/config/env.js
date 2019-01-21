e0d3b2b1284c0c5cc4e1ab8263775b09d62b8692
JasperLichte <contact@jasper-lichte.com>
2019-01-21 12:21:03 +0100

    Create environment constants

---
+++ src/config/env.js
@@ -0,0 +1,6 @@
+export const contributors = Object.freeze({
+  'JasperLichte': {name: 'Jasper Lichte', githubUrl: 'https://github.com/JasperLichte'},
+  'TaZn': {name: 'Harun Can', githubUrl: 'https://github.com/TaZn'},
+});
+
+export const repoUrl = 'https://github.com/JasperLichte/Rainbow-Cube';
