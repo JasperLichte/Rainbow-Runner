55b9bfc9369be48cb78858f68407e27feb829fe1
Jasper Lichte <jlichte@hh.rexx-systems.rx>
2019-02-11 13:33:44 +0100

    Add player object to level3

--- src/levels/levels/level3.js
+++ src/levels/levels/level3.js
@@ -1,11 +1,9 @@
 import symbols from './../symbols.js';
 
 const w = symbols['wall'];
-const c = symbols['coin'];
 const x = symbols['void'];
 const s = symbols['spikes'];
 const p = symbols['player'];
-const y = symbols['diamond'];
 
 export default [
     [x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,],
@@ -17,7 +15,7 @@ export default [
     [x, x, x, x, x, x, x, w, x, x, w, w, w, x, x, x, x, x, x, x, x, x, x, x,],
     [x, x, x, x, x, x, w, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,],
     [x, x, x, x, x, w, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,],
-    [x, x, x, x, w, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,],
+    [p, x, x, x, w, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,],
     [w, w, w, w, x, x, x, x, x, x, x, x, w, w, w, w, w, w, w, w, w, w, w, w,],
     [x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,],
 ];
