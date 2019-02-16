90e00d6829195964dd11b09e180e717d03a7179c
JasperLichte <contact@jasper-lichte.com>
2019-02-16 13:05:51 +0100

    Update

--- build/app.js
+++ build/app.js
@@ -1,1 +1,2 @@
 import './bootstrap.js';
+console.log(1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './build/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'assets')
