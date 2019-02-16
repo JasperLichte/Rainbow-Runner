d42641adbc21c34bad94c1e92690efb8e5b47d58
JasperLichte <contact@jasper-lichte.com>
2019-02-16 12:49:17 +0100

    Remove ignored files

--- build/controls/keyboardControls.js
+++
@@ -1,7 +0,0 @@
-export var playerControls = Object.freeze({
-    'ArrowUp': { action: 'jump' },
-    'KeyD': { action: 'moveForward' },
-    'ArrowRight': { action: 'moveForward' },
-    'KeyA': { action: 'moveBackward' },
-    'ArrowLeft': { action: 'moveBackward' },
-});
--- build/func/funcs.js
+++
@@ -1,5 +0,0 @@
-export var calcGravity = function (m, g) { return (m / 100000) * g; };
-export var preventDefaultEvents = function () {
-    var preventDefault = function (e) { return e.preventDefault(); };
-    window.addEventListener('keydown', function (e) { return e.code === 'Space' && preventDefault(e); });
-};
