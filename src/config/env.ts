1c5c02b51750789710cd0973d1f12090e260d83e
JasperLichte <contact@jasper-lichte.com>
2019-02-16 16:38:08 +0100

    Add exception

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,11 @@
+import exceptions from './../errorhandling/exceptions.js';
+
 // @ts-ignore
 const conf = window.CONFIG;
+
+if (!conf) {
+  throw new Error(exceptions['UNEXPECTED_SERVER_BEHAVIOR']);
+}
 
 export const APP_NAME = conf['APP_NAME'];
 export const PRODUCTION = !!conf['PRODUCTION'];
--- src/errorhandling/exceptions.ts
+++ src/errorhandling/exceptions.ts
@@ -1,4 +1,5 @@
 export default Object.freeze({
+  'UNEXPECTED_SERVER_BEHAVIOR': 'Server behaved differently than expected',
   'INVALID_RENDER_OBJECT': 'Render object not provided',
   'INVALID_POSITION_OBJECT': 'Invalid position object! Expected an object like {x, y}',
   'INVALID_LEVEL': 'Received invalid level. Expected an array of arrays of chars.',
