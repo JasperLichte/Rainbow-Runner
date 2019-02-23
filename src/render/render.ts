d0e65a14e5605e72102540aac130960e83aa7360
JasperLichte <contact@jasper-lichte.com>
2019-02-23 11:25:03 +0100

    Refactor

--- src/render/render.ts
+++ src/render/render.ts
@@ -3,10 +3,10 @@ import AnimationLoop from './AnimationLoop.js';
 
 export default (engineHelper: RenderEngineHelper, level: string[][]) => {
 
-  const animationLoopHelper = new AnimationLoop(engineHelper, level);
+  const animationLoop = new AnimationLoop(engineHelper, level);
   AnimationLoop.start();
   (function animate() {
-    animationLoopHelper.update();
+    animationLoop.update();
 
     requestAnimationFrame(animate);
   })();
