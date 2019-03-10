bf124a796181952a2c07ed7fa0d5486a84d30194
JasperLichte <contact@jasper-lichte.com>
2019-03-10 10:34:58 +0100

    Fix menu

--- src/ui/Menu.ts
+++ src/ui/Menu.ts
@@ -35,7 +35,7 @@ export default class Menu {
     if (Math.random() > 0.5) {
       this.domElement.style.left = '-100vw';
     } else {
-      this.domElement.style.left = '200vw';
+      this.domElement.style.left = '100vw';
     }
     this.hasDoneInitialRender && AnimationLoop.start();
     return this;
