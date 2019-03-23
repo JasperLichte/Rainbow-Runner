75656fe010ca81b004b5c7581a4fcc80e962b6d8
JasperLichte <contact@jasper-lichte.com>
2019-03-23 18:15:28 +0100

    Minor refactoring

--- src/func/funcs.ts
+++ src/func/funcs.ts
@@ -3,6 +3,14 @@ export const calcGravity = (m: number, g: number): number => (m / 100000) * g;
 export const preventDefaultEvents = (): void => {
   const preventDefault = e => e.preventDefault();
 
+  const eventsToPreventDefaultOn = Object.freeze([
+    // 'contextmenu',
+  ]);
+
+  eventsToPreventDefaultOn.forEach(eType => {
+    window.addEventListener(eType, preventDefault);
+  });
+
   const keysToPreventDefaultOn = Object.freeze([
     'Space',
   ]);
