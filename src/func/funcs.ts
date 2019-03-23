64ceb5b7f5c7e8f3607dcf4c674e0742d12fa71e
JasperLichte <contact@jasper-lichte.com>
2019-03-23 18:11:44 +0100

    Minor refactoring

--- src/func/funcs.ts
+++ src/func/funcs.ts
@@ -1,6 +1,15 @@
-export const calcGravity = (m, g): number => (m / 100000) * g;
+export const calcGravity = (m: number, g: number): number => (m / 100000) * g;
 
 export const preventDefaultEvents = (): void => {
   const preventDefault = e => e.preventDefault();
-  window.addEventListener('keydown', e => e.code === 'Space' && preventDefault(e));
+
+  const keysToPreventDefaultOn = Object.freeze([
+    'Space',
+  ]);
+
+  window.addEventListener('keydown', e => {
+    if (keysToPreventDefaultOn.includes(e.code)) {
+      preventDefault(e);
+    }
+  });
 };
